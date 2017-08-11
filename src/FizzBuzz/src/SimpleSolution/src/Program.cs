29c85c9de2ce86fd1b492c0dfb703a9e4cac95c8
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-08-11 15:20:25 -0500

    Initial Commit

---
+++ src/FizzBuzz/src/SimpleSolution/src/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SimpleSolution {
+
+    class Program {
+
+        static void Main(string[] args) {
+            for (int i = 1; i <= 100; ++i) {
+                string msg = String.Empty;
+                if (i % 3 == 0)
+                    msg += "Fizz";
+
+                if (i % 5 == 0)
+                    msg += "Buzz";
+
+                if (!String.IsNullOrEmpty(msg))
+                    Console.WriteLine(msg);
+                else
+                    Console.WriteLine(i);
+            }
+        }
+
+    }
+
+}
---
+++ src/FizzBuzz/src/VisitorPattern/src/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace src {
+
+    class Program {
+        
+        static void Main(string[] args) {
+            /*
+             * Here is the visitor pattern.  The FizzBuzzCreator type is "accepting" three different
+             * "vistitors" and the visitors do something at certain points of an iteration.  I'm simplifying
+             * the pattern by using Action<int> instead of creating visitor classes that implement an interface
+             * and a whole other mess of things.
+             */
+            FizzBuzzCreator creator = new FizzBuzzCreator(
+                (number) => Console.WriteLine($"Fizz\t->\t{number}"),
+                (number) => Console.WriteLine($"Buzz\t->\t{number}"),
+                (number) => Console.WriteLine($"FizzBuzz->\t{number}"),
+                1,
+                100
+            );
+            creator.Create();
+        }
+
+    }
+
+    internal class FizzBuzzCreator {
+
+        private Action<int> _onDivisibleByThree;
+        private Action<int> _onDivisibleByFive;
+        private Action<int> _onDivisibleByBoth;
+        private int _upperBounds;
+        private int _lowerBounds;
+
+        public FizzBuzzCreator(
+            Action<int> onDivisibleByThree,
+            Action<int> onDivisibleByFive,
+            Action<int> onDivisibleByBoth,
+            int lowerBounds,
+            int upperBounds
+        ) {
+            _onDivisibleByBoth = onDivisibleByBoth;
+            _onDivisibleByThree = onDivisibleByThree;
+            _onDivisibleByFive = onDivisibleByFive;
+            _upperBounds = upperBounds;
+            _lowerBounds = lowerBounds;
+        }
+
+        public void Create() {
+            for (int i = _lowerBounds; i <= _upperBounds; ++i) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    _onDivisibleByBoth(i);
+                    continue;
+                }
+
+                if (i % 3 == 0) {
+                    _onDivisibleByThree(i);
+                    continue;
+                }
+
+                if (i % 5 == 0) {
+                    _onDivisibleByFive(i);
+                    continue;
+                }
+            }
+        }
+
+    }
+
+}
