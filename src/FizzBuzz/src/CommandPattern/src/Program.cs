aa7c8bba5ab90200318003a84876ce356a0c0f64
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-08-14 10:49:46 -0500

    Adding some comments.

--- src/FizzBuzz/src/CommandPattern/src/Program.cs
+++ src/FizzBuzz/src/CommandPattern/src/Program.cs
@@ -6,6 +6,18 @@ namespace CommandExample {
     class Program {
 
         static void Main(string[] args) {
+            /*
+             * Here is the command pattern.  The idea is pretty simple but the implementation can become
+             * a little intensive.  The idea is you have something that invokes commands, and commands
+             * themselves.  The commands are free to take in whatever is necessary as a parameter but 
+             * every command must implement an interface.  This decouples the entity that will invoke
+             * commands and the commands themselves.
+             *
+             * The command will store the logic of what needs to be done.  In this case determing what to
+             * print on the console window for whatever number gets passed in.  Whatever invokes these commands
+             * doesn't need to care what command it gets passed since every command implements the same
+             * interface.
+             */
             CommandInvoker invoker = new CommandInvoker();
             IList<ICommand> commands = new List<ICommand>();
 
