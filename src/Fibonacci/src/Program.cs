bc825516c946aaf2a7062420b6e11eb5488d43e5
Logan Komanetz <Logan.Komanetz@networkedenergy.com>
2017-08-16 21:11:39 -0500

    Adding recursive fibonacci sequence.

---
+++ src/Fibonacci/src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace src {
+
+    class Program {
+
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine($"Expected 1 argument.  {args.Length} arguments provided");
+                return;
+            }
+
+            int endNumber = Int32.Parse(args[0]);
+            for (int i = 0; i < endNumber; ++i) {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+
+        static int Fibonacci(int num) {
+            if (num == 0 || num == 1) {
+                return num;
+            }
+
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+
+    }
+
+}
